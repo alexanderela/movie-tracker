c3ccc39107d6c4ff9de2f37de6255999c6f9cdfe
Drake Bott <bott.da@gmail.com>
2018-10-24 15:54:40 -0600

    Create MainPage component

---
+++ src/components/MainPage/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import CardContainer from '../CardContainer';
+import { connect } from 'react-redux';
+import * as userActions from '../../actions/userActions';
+
+
+const MainPage = (props) => {
+  const handleSignOut = (event) => {
+    event.preventDefault();
+    props.signOut();
+  }
+  return (
+    <div className='MainPage'>
+      <button className='sign-out' onClick={handleSignOut}>Sign Out</button>
+      <CardContainer movies={props.movies}/>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  signOut: () => dispatch(userActions.signOut)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
