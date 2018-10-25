2cf361c76bb0862260d8fd4b29842e25465c587b
Drake Bott <bott.da@gmail.com>
2018-10-25 13:40:48 -0600

    Write tests for API calls

--- src/utilities/API.test.js
+++ src/utilities/API.test.js
@@ -1,35 +1,77 @@
 import * as API from './API';
 
-describe('loginUser', () => {
-  let mockUser;
-  beforeEach(() => {
-    mockUser = {email: 'tim@aol.com', password: 'password'};
-    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-      json: () => Promise.resolve({email: 'tim@aol.com', password: 'password'})
-    }))
+describe('API', () => {
+  describe ('fetchData', () => {
+    beforeEach(() => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        json: () => Promise.resolve({data: 'data here'})
+      }));
+    });
+
+    it('Should call fetch with the given url', () => {
+      const url = 'google.com';
+      API.fetchData(url);
+      expect(window.fetch).toHaveBeenCalledWith(url);
+    });
+
+    it('Should return json\'d response from fetch', async () => {
+      const url = 'google.com';
+      const expected = {data: 'data here'}
+      const response = await API.fetchData(url)
+      expect(response).toEqual(expected);
+    });
   });
 
-  it('should call fetch on path /api/users', () => {
-    API.loginUser(mockUser);
-    expect(window.fetch.mock.calls[0][0]).toBe('http://localhost:3000/api/users');
+  describe('loginUser', () => {
+    let mockUser;
+    beforeEach(() => {
+      mockUser = {email: 'tim@aol.com', password: 'password'};
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        json: () => Promise.resolve(
+          {data: {email: 'tim@aol.com', password: 'password'}})
+      }))
+    });
+
+    it('should call fetch on path /api/users', () => {
+      API.loginUser(mockUser);
+      expect(window.fetch.mock.calls[0][0]).toBe('http://localhost:3000/api/users');
+    });
+
+    it('should pass an options object with the stringified user data to fetch', () => {
+      const options = {
+          method: 'POST',
+          body: JSON.stringify(mockUser),
+          headers:{
+            'Content-Type': 'application/json'
+          }
+      }
+      API.loginUser(mockUser);
+      expect(window.fetch.mock.calls[0][1]).toEqual(options)
+    });
+
+    it('should return the json\'d user data from response', async () => {
+      const response = await API.loginUser(mockUser);
+      const expected = {email: 'tim@aol.com', password: 'password'}
+      expect(response).toEqual(expected);
+    });
   });
 
-  it('should pass an options object with the stringified user data to fetch', () => {
-    const options = {
+  describe('createUser', () => {
+    beforeEach(() => {
+      window.fetch = jest.fn();
+    })
+    it('Should call fetch with the correct arguments', () => {
+      const url = 'http://localhost:3000/api/users/new';
+      const mockUser = {name: 'Tim', email: 'tim@aol.com', password: 'password'}
+      const mockOptions = {
         method: 'POST',
         body: JSON.stringify(mockUser),
-        headers:{
+        headers: {
           'Content-Type': 'application/json'
         }
-    }
-    API.loginUser(mockUser);
-    expect(window.fetch.mock.calls[0][1]).toEqual(options)
+      }
+      API.createUser(mockUser);
+      expect(window.fetch).toHaveBeenCalledWith(url, mockOptions);
+    });
   });
-
-  it('should return the json\'d user data from response', async () => {
-    const response = await API.loginUser(mockUser);
-    const expected = {email: 'tim@aol.com', password: 'password'}
-    expect(response).toEqual(expected);
-  });
-
 });
