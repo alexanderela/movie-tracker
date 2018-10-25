a6d2487881a21111d1f3b7defb43e9b79bb8505d
Alexander Ela <ela.alexander@gmail.com>
2018-10-24 21:29:46 -0600

    Add logic to API.js to check for pre-existing users

--- src/utilities/API.js
+++ src/utilities/API.js
@@ -25,14 +25,36 @@ export const loginUser = async (user) => {
   }
 }
 
-export const createUser = (user) => {
-  fetch('http://localhost:3000/api/users/new', {
-    method: 'POST',
-    body: JSON.stringify(user),
+export const createUser = async (user) => {
+  const userData = await getUsers()
+  console.log(userData)
+  const matchingUser = userData.find(userEntry => {
+    return userEntry.email === user.email
+  })
+  if (!matchingUser) {
+    fetch('http://localhost:3000/api/users/new', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }); 
+  } else {
+    console.log('ERROR: User already exists')
+    return null
+  }
+}
+
+const getUsers = async () => {
+  const response = await fetch('http://localhost:3000/api/users', {
+    method: 'GET',
+    body: JSON.stringify(),
     headers:{
       'Content-Type': 'application/json'
     }
-  });
+  })
+  const userData = await response.json();
+  return userData.data
 }
 
 
