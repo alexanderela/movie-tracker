a9d0473362dc779fc496031ef06e4c2fc7b32115
Drake Bott <bott.da@gmail.com>
2018-10-25 15:43:54 -0600

    Test userReducer

---
+++ src/reducers/Tests/userReducer.test.js
@@ -0,0 +1,47 @@
+import { userReducer } from '../userReducer';
+
+describe('userReducer', () => {
+  const defaultState = {
+    id: null,
+    name: null,
+    email: null,
+    loggedIn: false
+  }
+
+  it('Should return default state when no type given', () => {
+    const newState = userReducer(undefined, {});
+  });
+
+  it('Should add user to state on SUCCESSFUL_LOGIN', () => {
+    const mockAction = {
+      type: 'SUCCESSFUL_LOGIN',
+      user: {
+        name: 'Tim',
+        email: 'tim@aol.com',
+        id: 1
+      }
+    }
+
+    const expected = {
+      id: 1,
+      name: 'Tim',
+      email: 'tim@aol.com',
+      loggedIn: true
+    }
+    const newState = userReducer(undefined, mockAction);
+
+    expect(newState).toEqual(expected);
+  });
+
+  it('Should remove user from state on SIGN_OUT', () => {
+    const state = {
+      id: 1,
+      name: 'Tim',
+      email: 'tim@aol.com',
+      loggedIn: false
+    }
+    const mockAction = { type: 'SIGN_OUT' }
+    const newState = userReducer(state, mockAction);
+    expect(newState).toEqual(defaultState);
+  });
+});
