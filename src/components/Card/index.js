5925a04600101bbc088fce1d86ed850027473d64
Drake Bott <bott.da@gmail.com>
2018-10-27 14:45:51 -0600

    Make Card into a class

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Card.css';
 import star from '../../images/star-clear.svg';
 import filledStar from '../../images/star.svg';
@@ -6,42 +6,48 @@ import { toggleFavorite, addFavorite, removeFavorite } from '../../actions/userA
 import { connect } from 'react-redux';
 import * as API from '../../utilities/API'
 
-const Card = ({ movie, user, favorites, toggleFavorite, addToFavorites, removeFromFavorites }) => {
-	const handleFavorite = async (movie) => {
+export class Card extends Component {
+	handleFavorite = async (movie) => {
+    console.log(movie);
+    const { user } = this.props;
 		const { id, favorite } = movie;
     if (favorite) {
       API.removeFavorite(movie, user);
     } else {
       API.addFavorite(movie, user);
     }
-    toggleFavorite(id);
+    this.props.toggleFavorite(id);
 	}
 
-	return(
-		<div className='Card' style={{backgroundImage: 'url(' + movie.backdrop + ')'}}>
-			<div className="card-inner-wrapper">
-				<h3 className="movie-title" >
-					<div className="movie-rating" >Rating {movie.rating}/10</div>
-					{movie.title}
-					<button
-						className={`card-favorite-button
-							${movie.favorite
-								? 'fav-btn-active'
-								: 'fav-btn-inactive'}`}
-						onClick={() => handleFavorite(movie)}
-					>
-						<img alt="" src={star} />
-					</button>
-				</h3>
-				{/* <img  */}
-					{/* src={movie.poster}  */}
-					{/* className='poster-image'/> */}
-				<p>{movie.overview}</p>
-				<p>Opens: {movie.releaseDate}</p>
-				<p>Viewer Rating: {movie.rating}</p>
-			</div>
-	  </div>
-  )
+  render() {
+    const { movie, toggleFavorite } = this.props;
+
+    return(
+      <div className='Card' style={{backgroundImage: 'url(' + movie.backdrop + ')'}}>
+        <div className="card-inner-wrapper">
+          <h3 className="movie-title" >
+            <div className="movie-rating" >Rating {movie.rating}/10</div>
+            {movie.title}
+            <button
+              className={`card-favorite-button
+                ${movie.favorite
+                  ? 'fav-btn-active'
+                  : 'fav-btn-inactive'}`}
+              onClick={() => this.handleFavorite(movie)}
+            >
+              <img alt="" src={star} />
+            </button>
+          </h3>
+          {/* <img  */}
+            {/* src={movie.poster}  */}
+            {/* className='poster-image'/> */}
+          <p>{movie.overview}</p>
+          <p>Opens: {movie.releaseDate}</p>
+          <p>Viewer Rating: {movie.rating}</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export const mapStateToProps = (state) => ({
