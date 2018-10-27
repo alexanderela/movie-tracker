869e3c2f9bf6610622375eae3a0f810b0ddbcc33
Drake Bott <bott.da@gmail.com>
2018-10-26 19:21:28 -0600

    Move app to components directory

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import { Route, Redirect } from 'react-router-dom';
-import MainPage from '../MainPage';
-import Login from '../Login';
+import MainPage from '../../containers/MainPage';
+import Login from '../../containers/Login';
 import './App.css';
 
 class App extends Component {
