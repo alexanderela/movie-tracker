b18b2ce0952a9d8c9d336eb6eb13165ff315ae4d
Drake Bott <bott.da@gmail.com>
2018-10-27 19:53:20 -0600

    Add switch to App routing to account for invalid paths

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setMovies } from '../../actions/movieActions';
 import * as userActions from '../../actions/userActions';
@@ -26,27 +26,29 @@ export class App extends Component {
 
     return (
       <div className='App'>
-        <Route exact path='/' render={() => <MainPage movies={movies}/>}/>
-        <Route exact path='/login' render={() => loggedIn ?
-          <Redirect to='/'/> : <Login/>}/>
-        <Route exact path='/favorites' render={() => {
-          if(!loggedIn) {
-            alert('Please create an account or login to select favorites')
-            return <Redirect to='/login'/>
-          } else {
-            return <MainPage movies={this.filterFavorites()}/>}
-          }
-        }/>
+        <Switch>
+          <Route exact path='/' render={() => <MainPage movies={movies}/>}/>
+          <Route exact path='/login' render={() => loggedIn ?
+            <Redirect to='/'/> : <Login/>}/>
+          <Route exact path='/favorites' render={() => {
+            if(!loggedIn) {
+              alert('Please create an account or login to select favorites')
+              return <Redirect to='/login'/>
+            } else {
+              return <MainPage movies={this.filterFavorites()}/>}
+            }
+          }/>
+        <Route render={() => <MainPage movies={movies}/>}/>
+      </Switch>
       </div>
     );
   }
 }
 
-export const mapStateToProps = ({ user, movies, favorites }) => ({ user, movies, favorites });
+export const mapStateToProps = ({ user, movies }) => ({ user, movies });
 
 export const mapDispatchToProps = (dispatch) => ({
   setMovies: (movies) => dispatch(setMovies(movies))
 });
-
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
