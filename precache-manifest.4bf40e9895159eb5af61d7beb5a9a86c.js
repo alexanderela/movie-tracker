5426b9215a010ab6c23a85c67646279c8c14827d
Alexander Ela <ela.alexander@gmail.com>
2018-11-14 15:36:19 -0700

    Updates

---
+++ precache-manifest.4bf40e9895159eb5af61d7beb5a9a86c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0210b84c1f37cac96edb73e3621355a",
+    "url": "/movie-tracker/static/media/star.c0210b84.svg"
+  },
+  {
+    "revision": "25bcd45fd6c272607e1465a5e02a4582",
+    "url": "/movie-tracker/static/media/star-clear.25bcd45f.svg"
+  },
+  {
+    "revision": "05a753d921fc48588e3079d581b85d47",
+    "url": "/movie-tracker/static/media/movies.05a753d9.jpg"
+  },
+  {
+    "revision": "f8858d24fd028d581c979ccb1921428c",
+    "url": "/movie-tracker/static/media/film-red.f8858d24.svg"
+  },
+  {
+    "revision": "f5a723b953ab703507c1",
+    "url": "/movie-tracker/static/js/runtime~main.f5a723b9.js"
+  },
+  {
+    "revision": "634e7c0e1f04d1c22fe7",
+    "url": "/movie-tracker/static/js/main.634e7c0e.chunk.js"
+  },
+  {
+    "revision": "24048e9b73b82a33612c",
+    "url": "/movie-tracker/static/js/1.24048e9b.chunk.js"
+  },
+  {
+    "revision": "634e7c0e1f04d1c22fe7",
+    "url": "/movie-tracker/static/css/main.5ef51d02.chunk.css"
+  },
+  {
+    "revision": "fe7d5600d893e0cf86f7c3c2898c8ac6",
+    "url": "/movie-tracker/index.html"
+  }
+];
